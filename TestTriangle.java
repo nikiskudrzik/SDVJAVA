57023f6a9fa241ba742b847a58821c7cb8c46819
nikiskudrzik <nskudrzik@ivytech.edu>
2026-01-26 21:32:06 -0500

    Add files via upload

---
+++ TestTriangle.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TestTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter three sides: ");
+        double side1 = input.nextDouble();
+        double side2 = input.nextDouble();
+        double side3 = input.nextDouble();
+
+        System.out.print("Enter a color: ");
+        String color = input.next();
+
+        System.out.print("Is the triangle filled? (true/false): ");
+        boolean filled = input.nextBoolean();
+
+        Triangle triangle = new Triangle(side1, side2, side3);
+        triangle.setColor(color);
+        triangle.setFilled(filled);
+
+        System.out.println(triangle.toString());
+        System.out.println("Area: " + triangle.getArea());
+        System.out.println("Perimeter: " + triangle.getPerimeter());
+        System.out.println("Color: " + triangle.getColor());
+        System.out.println("Filled: " + triangle.isFilled());
+
+        input.close();
+    }
+}
